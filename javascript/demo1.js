2961e2ad2b002254699c928768b7507e87856276
= <=>
2023-03-08 22:05:14 +0530

    commit on 8/03/2023 update

---
+++ javascript/demo1.js
@@ -0,0 +1,5 @@
+"use strict";
+var v1 = "Welcome";
+let v2 = " to";
+const v3 = " Typescript";
+console.log(v1, v2, v3);
---
+++ typescript/demo1.ts
@@ -0,0 +1,5 @@
+var v1:string="Welcome";
+let v2:string=" to";
+const v3:string=" Typescript";
+
+console.log(v1,v2,v3);
